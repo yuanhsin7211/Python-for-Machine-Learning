4fb2c49ac4fe26f279fd6169c47fc6c6d5ebcc9b
Yuan-Hsin Huang <yuanhsin7211@gmail.com>
2016-09-14 16:33:10 -0700

    Update and rename BasicDataOverview.py to LinearRegression.py

---
+++ LinearRegression.py
@@ -0,0 +1,58 @@
+## Import the Libraries ##
+import pandas as pd
+import matplotlib.pyplot as plt
+# plot only appears in the jupyter notebook
+%matplotlib inline
+
+## Read data into a DataFrame ##
+data = pd.read_csv('http://www-bcf.usc.edu/~gareth/ISL/Advertising.csv',index_col=0)
+data.head()
+data.shape
+
+##Plot the relationship ##
+fig, axs = plt.plot(1,3,sharey=True)
+data.plot(kind='scatter',x='TV',y='Sales',ax=axs[0],figsize=(16,8))
+data.plot(kind='scatter',x='Radio',y='Sales',ax=axs[1])
+data.plot(kind='scatter',x='Newspaper',y='Sales',ax=axs[2])
+
+# ~Simple Linear Regression~ #
+
+## Method 1 : Statsmodels  ##
+
+import statsmodels.formula.api as smf
+lm = smf.ols('Sales ~ TV',data = data).fit()
+lm.params
+
+
+X_new = pd.DataFrame({'TV':[50]}) #Statasmodels always use dataframe type as the predicted 
+X_new.head()
+lm.predict(X_new)
+
+X_new = pd.DataFrame({'TV':[data.TV.min(),data.TV.max()]})
+X_new.head()
+
+preds = lm.predict(X_new)
+preds
+
+# plot the observed data
+data.plot(kind='scatter',x='TV',y='Sales')
+# plot the least square line
+plt.plot(X_new,preds,c='red',linewidth=2)
+
+lm.conf_int()
+lm.pvalues
+
+# how well the model fit- R-squared
+lm.rsquared
+
+# ~Multiple Linear Regression~ #
+lm = smf.ols(formula = 'Sales ~ TV + Radio + Newspaper' ,data=data).fit()
+lm.params
+lm.summary()
+
+## Method 2 : Scikit-Learn  ##
+
+
+
+
+
